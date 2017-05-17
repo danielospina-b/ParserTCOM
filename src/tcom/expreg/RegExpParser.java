27e92f47f44c8f3063f2a8bf999d76539dda9a33
Daniel Ospina <daniel.ospina-b@mail.escuelaing.edu.co>
2017-05-17 11:10:30 -0500

    Solucionado problema con *, falta que abc* = ab(c*) y != de (abc*)

--- src/tcom/expreg/RegExpParser.java
+++ src/tcom/expreg/RegExpParser.java
@@ -65,36 +65,44 @@ public class RegExpParser implements ParserTreeI {
     public AstI parse() throws RegParsingException{
         token = lexer.nextToken();
         AstTree tree = pExp();
-        //if (token.getType() != EOF) throw new RegParsingException("ERROR: Columna " + token.getCharPositionInLine() + ", Se esperaba carácter de final de línea");
-        System.out.println("TREE " + tree.getName() + " " + tree.getChild(0).getName());
+        System.out.println("POST tree = pExp() en parse");
+        if (token.getType() != EOF) throw new RegParsingException("ERROR: Columna " + token.getCharPositionInLine() + ", Se esperaba carácter de final de línea pero se encontró el carácter: " + token.getText());
+        System.out.println("TREE FINISHED");
         return tree;
     }
 
     public AstTree pExp() throws RegParsingException {
+        System.out.println("tcom.expreg.RegExpParser.pExp()");
         AstTree first = pExpMinima();
+        System.out.println("FIRST == NULL? : " + (first == null) + " first.getName(): " + first.getName());
         AstTree res = null;
         AstTree brothers = new AstTree(new CommonToken(2, "."));
         brothers.addChild(first);
-        while (token.getType() == LPAR || token.getType() == LAMBDA || token.getType() == OR) {
+        System.out.println("PostAddChild");
+        while (token.getType() == LPAR || token.getType() == LAMBDA || token.getType() == OR || token.getType() == SYMBOL) {
             if (token.getType() == OR) {
                 if (res == null) res = new AstTree(token);
                 token = lexer.nextToken();
                 System.out.println("Res.addchild(pexpminima) con token actual = " + token.getText());
                 AstTree value = pExpMinima();
-                System.out.println("name de value: " + value.getChild(0).getName());
+                System.out.println("name de value: " + value.getName());
                 res.addChild(value);
             }
-            if (token.getType() == LPAR) {
+            if (token.getType() == LPAR || token.getType() == LAMBDA || token.getType() == SYMBOL) {
                 brothers.addChild(pExpMinima());
             }
         }
         if (res == null) {
+            System.out.println("entro por res == NULL");
             if (brothers.getChildCount() > 1) {
+                System.out.println("Entro por childcount");
                 return brothers;
             }
+            System.out.println("Return first");
             return first;
         }
         else {
+            System.out.println("entro por res != null");
             res.insertChild(first, 0);
             return res;
         }
@@ -103,24 +111,39 @@ public class RegExpParser implements ParserTreeI {
     public AstTree pExpMinima() throws RegParsingException {
         System.out.println("tcom.expreg.RegExpParser.pExpMinima()");
         if (token.getType() == LAMBDA) {
+            AstTree res = new AstTree(token);
+            System.out.println("PRENEXTTOKEN");
             token = lexer.nextToken();
-            return new AstTree(token);
+            System.out.println("POSNEXTTOKEN");
+            return res;
         }
         else if (token.getType() == LPAR) {
             System.out.println("Call por lpar en expminima");
             token = lexer.nextToken();
             AstTree tree = pExp();
-            if (token.getType() != RPAR) throw new RegParsingException("ERROR: Columna " + token.getCharPositionInLine() + ", Se esperaba un paréntesis derecho (RPAR)");
+            if (token.getType() != RPAR) throw new RegParsingException("ERROR: Columna " + token.getCharPositionInLine() + ", Se esperaba un paréntesis derecho (RPAR) pero se encontró el carácter: " + token.getText());
             System.out.println("Parentesis Derecho Salio ");
             token = lexer.nextToken();
-            return tree;
+            AstTree parent;
+            if (token.getType() == STAR) {
+                System.out.println("Estrellitaaa donde estaaas!!!!!!!!!!");
+                parent = new AstTree(token);
+                parent.addChild(tree);
+                token = lexer.nextToken();
+            }
+            else {
+                parent = tree;
+            }
+            return parent;
         }
         else if (token.getType() == SYMBOL){
             AstTree parent;
             AstTree tree = pPalabra();
             if (token.getType() == STAR) {
+                System.out.println("Estrella!!!!!!!!!!");
                 parent = new AstTree(token);
                 parent.addChild(tree);
+                token = lexer.nextToken();
             }
             else {
                 parent = tree;
@@ -129,7 +152,8 @@ public class RegExpParser implements ParserTreeI {
         }
         else {
             System.out.println("Token: " + token.getText());
-            throw new RegParsingException("Error que no tengo una puta idea");
+            throw new RegParsingException("ERROR: Columna: " + token.getCharPositionInLine() + ", Carácter inesperado, se esperaba LAMBDA o \n"
+                    + "PARENTESIS IZQUIERDO (LPAR) o Un carácter alfanumérico (SYMBOL) pero se encontró el carácter: " + token.getText());
         }
     }
     
@@ -138,7 +162,7 @@ public class RegExpParser implements ParserTreeI {
         AstTree tree = new AstTree(new CommonToken(2, "."));
         boolean isSymbol = true;
         System.out.println("Token: " + token.getText() + " Column: " + token.getCharPositionInLine());
-        if (token.getType() != SYMBOL) throw new RegParsingException("ERROR: Columna " + token.getCharPositionInLine() + ", Se esperaba un símbolo alfanumérico (SYMBOL)");
+        if (token.getType() != SYMBOL) throw new RegParsingException("ERROR: Columna " + token.getCharPositionInLine() + ", Se esperaba un símbolo alfanumérico (SYMBOL) pero se encontró el carácter: " + token.getText());
         while (isSymbol)  {
             tree.addChild(pSymbol());
             token = lexer.nextToken();
